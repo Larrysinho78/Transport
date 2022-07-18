7131b4e6d1b72406f1de0333625d2d38bf4b1f01
LarryMallea <larrymallea@gmail.com>
2022-07-18 09:05:44 -0400

    Creacion Tablas

---
+++ database/migrations/2022_07_18_130043_create_products_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('Res_Adm');
+            $table->string('Reg_Operador');
+            $table->string('Categoría');
+            $table->string('Operador');
+            $table->string('Propietario');
+            $table->string('Marca');
+            $table->string('Modelo');
+            $table->string('Capacidad');
+            $table->string('Clase');
+            $table->string('Placa');
+            $table->string('Rutas_Aut');
+            $table->date('Fecha_Emision');
+            $table->date('Fecha_Inicio');
+            $table->date('Fecha_Final');
+            $table->string('Estado')->default('Nuevo');       /* Nuevo_Renovación */
+            $table->string('Qr');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
